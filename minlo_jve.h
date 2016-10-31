4ac06c4049a35ced53d34b3472e609b00e2e0b6b
Daniel Quill <daniel.quill.14@ucl.ac.uk>
2016-10-31 09:15:20 +0000

    Updating with the latest files from GZ (although I'm pretty sure they are identical to what we already had)

--- minlo_jve.h
+++ minlo_jve.h
@@ -2,7 +2,7 @@ c -*- Fortran -*-
 c contains minlo options to use ptj rather than ptH 
 
       logical minlo_jve
-      double precision minlo_deltaR 
+      double precision minlo_deltaR
 
       common/cminlo_jve/minlo_deltaR,minlo_jve
-      save /cminlo_jve/ 
+      save /cminlo_jve/
